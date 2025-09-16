c3df67d2993e71a3f686a8faf95ba360521f8cf8
boburmirzo-brm <boburmirzo.brm@gmail.com>
2025-09-16 13:26:38 +0500

    test 2

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -1,35 +1,46 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-import { memo, useEffect, useState } from "react";
+import { memo } from 'react';
 
-const Verify = () => {
-  const param = useSearchParams();
-  const [error, setError] = useState<null | string>(null)
-
-  useEffect(() => {
-    const postData = async () => {
-      const res = await fetch("/api/auth/login", {
-        method: "POST",
-        body: JSON.stringify({
-          username: param.get("email"),
-          password: param.get("password"),
-        }),
-      });
-      if (!res.ok) {
-        setError("xato")
-        return;
-      }
-      window.location.href = "/about";
-    };
-    postData();
-  }, []);
-
+const Page = () => {
   return (
     <div className="Page">
-        {error && <p>{error}</p>}
-      <h2>Verify Page</h2>
+      <h2>Page</h2>
     </div>
   );
 };
 
-export default memo(Verify);
+export default memo(Page);
+// "use client";
+// import { useSearchParams } from "next/navigation";
+// import { memo, useEffect, useState } from "react";
+
+// const Verify = () => {
+//   const param = useSearchParams();
+//   const [error, setError] = useState<null | string>(null)
+
+//   useEffect(() => {
+//     const postData = async () => {
+//       const res = await fetch("/api/auth/login", {
+//         method: "POST",
+//         body: JSON.stringify({
+//           username: param.get("email"),
+//           password: param.get("password"),
+//         }),
+//       });
+//       if (!res.ok) {
+//         setError("xato")
+//         return;
+//       }
+//       window.location.href = "/about";
+//     };
+//     postData();
+//   }, []);
+
+//   return (
+//     <div className="Page">
+//         {error && <p>{error}</p>}
+//       <h2>Verify Page</h2>
+//     </div>
+//   );
+// };
+
+// export default memo(Verify);

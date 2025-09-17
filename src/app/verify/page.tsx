9a628c3c8c2b5206516980078b07dccd2fe06fae
boburmirzo-brm <boburmirzo.brm@gmail.com>
2025-09-17 07:37:55 +0500

    verify page

--- src/app/verify/page.tsx
+++ src/app/verify/page.tsx
@@ -1,46 +1,47 @@
-import { memo } from 'react';
+"use client";
+import { useSearchParams } from "next/navigation";
+import { memo, useEffect, useState } from "react";
 
-const Page = () => {
+const Verify = () => {
+  const param = useSearchParams();
+  const [error, setError] = useState<null | string>(null);
+
+  useEffect(() => {
+    const postData = async () => {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        body: JSON.stringify({
+          username: param.get("email"),
+          password: param.get("password"),
+        }),
+      });
+      if (!res.ok) {
+        setError("xato");
+        return;
+      }
+      window.location.href = "/about";
+    };
+    postData();
+  }, []);
+
   return (
     <div className="Page">
-      <h2>Page</h2>
+      {error && <p>{error}</p>}
+      <h2>Verify Page</h2>
     </div>
   );
 };
 
-export default memo(Page);
-// "use client";
-// import { useSearchParams } from "next/navigation";
-// import { memo, useEffect, useState } from "react";
+export default memo(Verify);
 
-// const Verify = () => {
-//   const param = useSearchParams();
-//   const [error, setError] = useState<null | string>(null)
+// import { memo } from 'react';
 
-//   useEffect(() => {
-//     const postData = async () => {
-//       const res = await fetch("/api/auth/login", {
-//         method: "POST",
-//         body: JSON.stringify({
-//           username: param.get("email"),
-//           password: param.get("password"),
-//         }),
-//       });
-//       if (!res.ok) {
-//         setError("xato")
-//         return;
-//       }
-//       window.location.href = "/about";
-//     };
-//     postData();
-//   }, []);
-
+// const Page = () => {
 //   return (
 //     <div className="Page">
-//         {error && <p>{error}</p>}
-//       <h2>Verify Page</h2>
+//       <h2>Page</h2>
 //     </div>
 //   );
 // };
 
-// export default memo(Verify);
+// export default memo(Page);
